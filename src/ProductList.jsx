7d5f68f35aacc5243489f743a2966772c8f7ea46
Deden Alfathimy <dedenhabibi@gmail.com>
2024-11-18 05:45:51 -0500

    Post-Review: Add item's price on ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -318,6 +318,7 @@ function ProductList() {
                                     <div className="product-card" key={plantIndex}>
                                         <img className="product-image" src={plant.image} alt={plant.name} />
                                         <div className="product-title">{plant.name}</div>
+                                        <div className="product-price">{plant.cost}</div>
                                         {
                                             addedToCart[plant.name]
                                                 ? <button className="product-button added-to-cart">Added to Cart</button>
